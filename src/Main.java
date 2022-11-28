b21317ce0d0e0d59af572398e02c7f63e386b112
Анастасия <tasyaosipova736@gmail.com>
2022-11-28 20:53:58 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,28 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     public static void task1 () {
         System.out.println("Задача 1");
         var dog = 8;
         var cat = 3.6;
         var paper = 763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        System.out.println("dog = " + dog);
+        System.out.println("cat = " + cat);
+        System.out.println("paper = " + paper);
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println("dog = " + dog);
+        System.out.println("cat = " + cat);
+        System.out.println("paper = " + paper);
     }
 }
